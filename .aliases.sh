1420e172ad09236b698d3ebde7ace912ed69a029
thomy <ttohmas@gmail.com>
2016-10-18 21:28:59 +0200

    update alias

--- .aliases.sh
+++ .aliases.sh
@@ -2,3 +2,6 @@ alias swp2='mvn clean package -DskipTests=true embedded-glassfish:run'
 alias ydl="youtube-dl -x -o '%(title)s.%(ext)s'" 
 alias vi="vim"
 alias pac="sudo pacman -S"
+alias gitls="git log --pretty=format:'%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]' --decorate"
+alias gittree="git log --graph --oneline --decorate --all"
+
