8cb0a82cb30a08b8b731327d3d01fc9184344229
Thomas Tannous <tannous@uni-bremen.de>
2016-11-03 17:30:42 +0100

    update' g

--- .aliases.sh
+++ .aliases.sh
@@ -5,3 +5,11 @@ alias pac="sudo pacman -S"
 alias gitls="git log --pretty=format:'%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]' --decorate"
 alias gittree="git log --graph --oneline --decorate --all"
 
+#git aliases
+alias gs="git status"
+alias gd="git diff"
+alias gdh="git diff HEAD"
+alias gc="git clone $1 $2"
+alias gcm="git commit -m "$1""
+alias gaa="git add -A ."
+alias gpo="git push origin $1"
